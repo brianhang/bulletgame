c3a02ac28080b8fb4af95bf5f40b9bbc6888ea43
Brian Hang <hangwithbrian@gmail.com>
2016-12-10 15:54:27 -0800

    Added socket connection between client and server

--- server/index.js
+++ server/index.js
@@ -3,5 +3,14 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+// Start the webserver for the game on localhost:8080
 app.use(express.static(__dirname + "/../client"))
-app.listen(8080)
+http.listen(8080)
+
+var players = [];
+
+// Add a player to the game when someone joins.
+io.on("connection", function(socket) {
+    console.log("Bloop")
+    console.log(players[0] === null);
+})
