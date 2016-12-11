09319dc686ab0954c43562ad9beddeb0747261bf
Aviscer <aviscerality@gmail.com>
2016-12-10 17:12:32 -0800

    Resizable Window

--- client/js/game.js
+++ client/js/game.js
@@ -1,5 +1,26 @@
 var socket = io();
 
 $(document).ready(function() {
-    alert(PIXI)
+	initialize();
 })
+
+function initialize() {
+	var renderer = new PIXI.WebGLRenderer(512, 512);
+
+	renderer.backgroundColor = 0x048542;
+	renderer.view.style.position = "absolute";
+	renderer.view.style.display = "block";
+	renderer.autoResize = true;
+	renderer.resize($(document).width(), $(document).height());
+	
+
+	document.body.appendChild(renderer.view);
+
+	var stage = new PIXI.Container();
+
+	renderer.render(stage);
+
+	window.addEventListener("resize", function(event){
+		renderer.resize($(document).width(), $(document).height());
+	});
+}
