07001d4ab70636a52c1eef0c2f05537fcdd21a8f
Aviscer <aviscerality@gmail.com>
2016-12-11 02:10:49 -0800

    Added functions that received and send data to server

--- client/js/game.js
+++ client/js/game.js
@@ -43,6 +43,21 @@ function render() {
 	renderer.render(stage);
 }
 
+// Called when mouse is moved
+$(document).mousemove(function(event) {
+	// Check if player sprite exists to turn
+	if (player.sprite !== undefined) {
+
+		// Get the angle of mouse from the sprite	
+		var angle = Math.atan2(event.pageY - player.sprite.position.y, 
+							   event.pageX - player.sprite.position.x);
+
+		// Send heading information to server
+		socket.emit("turn", angle); 
+	}
+});
+
+
 // Called when a player joins a game
 socket.on("join", function(data, isLocalPlayer) {
 	var client;
@@ -53,24 +68,50 @@ socket.on("join", function(data, isLocalPlayer) {
 		client = new Player(-1, 0, 0);
 	}
 
+	// Copy information from server
 	client.id = data.id;
-	client.x = data.x;
-	client.y = data.y;
-	client.heading = data.heading || 0;
+	client.heading = Math.random()*360//data.heading || 0;
 	client.thrust = data.thrust || 0;
 
+	// Set 
 	players[client.id] = client;
 
 	// Load ship textures
 	client.sprite = PIXI.Sprite.fromImage("assets/images/ship.png");
+	
 	// Set coordinates
-	client.sprite.position.x = client.x;
-	client.sprite.position.y = client.y;
+	client.sprite.position.x = data.x;
+	client.sprite.position.y = data.y;
+	client.sprite.rotation = client.heading;
+
+	// Put sprite is in middle of position
+	client.sprite.anchor.set(0.5, 0.5);
 
 
 	stage.addChild(client.sprite);
 });
 
+// Called when a player leaves the game
 socket.on("leave", function(id) {
+	// Delete player's sprite
+	stage.removeChild(players[id].sprite);
 	delete players[id];
+})
+
+// Called when a player changes position
+socket.on("move", function(id, x, y) {
+	var other = players[id];
+	other.x = data.x;
+	other.y = data.y;
+
+	stage.addChild(other.sprite);
+})
+
+// Called when a player changes angles
+socket.on("turn", function(id, angle) {
+	var other = players[id];
+	other.heading = data.heading;
+
+	stage.addChild(other.sprite);
+
 })
