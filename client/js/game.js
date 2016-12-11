38a51ea8f1ee7c38022a0c4e6f7300cceeab868a
Aviscer <aviscerality@gmail.com>
2016-12-10 23:33:25 -0800

    Added code to draw the ship sprite

--- client/js/game.js
+++ client/js/game.js
@@ -1,26 +1,50 @@
 var socket = io();
 
+let player = new Player(0, 0, 0);
+
 $(document).ready(function() {
 	initialize();
 })
 
 function initialize() {
+	// Create the renderer
 	var renderer = new PIXI.WebGLRenderer(512, 512);
 
-	renderer.backgroundColor = 0x048542;
+	// Background
+	renderer.backgroundColor = 0x1a1a1a;
 	renderer.view.style.position = "absolute";
 	renderer.view.style.display = "block";
 	renderer.autoResize = true;
 	renderer.resize($(document).width(), $(document).height());
 	
-
+	// Add canvas 
 	document.body.appendChild(renderer.view);
 
+	// Create container
 	var stage = new PIXI.Container();
 
 	renderer.render(stage);
 
+	// Resize whenever window is resized
 	window.addEventListener("resize", function(event){
 		renderer.resize($(document).width(), $(document).height());
 	});
+
+	// Load Assets into texture cache
+	PIXI.loader
+		.add("assets/images/ship.png")
+		.add("assets/images/bullet.png")
+		.load(setup);
+
+	function setup() {
+		var shipSprite = new PIXI.Sprite(
+			PIXI.loader.resources["assets/images/ship.png"].texture
+		);
+	
+		// Draw ship
+		stage.addChild(shipSprite);
+		renderer.render(stage);
+	}
+	
+
 }
